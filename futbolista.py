26b1ffd37def314df5380cb713c7aa077118c22a
sahelyromero <sahromeroca@unal.edu.co>
2023-10-15 15:37:08 -0500

    intento 4

--- futbolista.py
+++ futbolista.py
@@ -40,4 +40,4 @@ class Futbolista(Deportista, Persona):
 
     def __str__(self):
         
-        return f"Mi nombre es {self.getNombre} soy profesional en el deporte {self.getDeporte} Tengo {self.getEdad()} años de edad y llevo {self.getAñosPracticando} años en el deporte"
+        return f"Mi nombre es {self.getNombre} soy profesional en el deporte {self.getDeporte} Tengo {str(self.getEdad())} años de edad y llevo {str(self.getAñosPracticando)} años en el deporte"
